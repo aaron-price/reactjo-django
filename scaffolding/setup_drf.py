8786bb983c973cf797fe1ee86e60edd85777359f
Aaron Price <coding.aaronp@gmail.com>
2017-08-24 22:13:19 -0700

    Fix paths

--- scaffolding/setup_drf.py
+++ scaffolding/setup_drf.py
@@ -1,8 +1,14 @@
 from helpers.cli import manage
 from helpers.file_manager import file_manager as f
 from helpers.path_manager import path_manager as p
+from helpers.config_manager import get_cfg
+import os
 
 def setup_drf():
+    cfg = get_cfg()
+    project_name = cfg['project_name']
+
+    os.chdir(p('backend_project'))
     manage(['startapp', 'api'])
     f(p('serializers.py'), 'w', '')
     f(p('permissions.py'), 'w', '')
