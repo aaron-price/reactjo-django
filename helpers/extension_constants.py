69ca978eab6d537870f0fde0c641b0233d951ce0
Aaron Price <coding.aaronp@gmail.com>
2017-09-03 15:44:07 -0700

    Update comment

--- helpers/extension_constants.py
+++ helpers/extension_constants.py
@@ -2,5 +2,5 @@
 # file_manager('$out/some/path') == 'super_root/project_path/backend/some/path'
 OUTPUT_HOME = 'backend'
 
-# file_manager('$rc/assets') == 'super_root/reactjorc/extensions/django_trial/assets'
+# file_manager('$rc/assets') == 'super_root/reactjorc/extensions/reactjo_django/assets'
 RC_HOME = 'reactjo_django'
