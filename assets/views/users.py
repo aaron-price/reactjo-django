66b50e85e2a020c410f54b62aec4e796dd1cc325
Aaron Price <coding.aaronp@gmail.com>
2017-09-04 12:40:57 -0700

    Fix TokenAuthentication class

--- assets/views/users.py
+++ assets/views/users.py
@@ -2,7 +2,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handles creating, reading, and updating profiles."""
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    authentication_classes = (authentication.TokenAuthentication,)
+    authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
