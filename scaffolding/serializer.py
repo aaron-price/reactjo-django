e75504c16fc42dd1382620dab908bc994afded85
Aaron Price <coding.aaronp@gmail.com>
2017-09-09 15:48:13 -0700

    Wrap serializer fields in quotes

--- scaffolding/serializer.py
+++ scaffolding/serializer.py
@@ -2,6 +2,9 @@ from helpers.config_manager import get_cfg, set_cfg
 from helpers.worklist import worklist as wl
 from helpers.file_manager import file_manager as f
 
+def quote(string):
+    return f"'{string}'"
+
 def scaffold_serializer():
     cfg= get_cfg()
     fields = ''
@@ -9,9 +12,9 @@ def scaffold_serializer():
         # Get just the title
         title = field.replace('\n', '').replace('\t', '').split(' ')[0]
         if i == 0:
-            fields += title
+            fields += quote(title)
         else:
-            fields += ', ' + title
+            fields += ', ' + quote(title)
 
     new_serializer = f('$assets/serializers/new.py', 'r').format(
         Title = cfg['current_scaffold']['model']['Title'],
