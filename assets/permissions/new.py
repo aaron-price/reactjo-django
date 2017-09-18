bf2a22cc2746465ef3cf622121d4ad75ea184ddb
Aaron Price <coding.aaronp@gmail.com>
2017-09-17 18:17:39 -0700

    Fix content permissions for details

--- assets/permissions/new.py
+++ assets/permissions/new.py
@@ -1,17 +1,19 @@
 class {Model}Permissions(permissions.BasePermission):
     message = "You do not have permission."
     def has_permission(self, request, view):
-        if request.method == 'POST':
-            return check_allowed([{post_users}], request, obj = None)
-        elif request.method == 'GET':
-            return check_allowed([{get_users}], request, obj = None)
+        if request.method == 'GET':
+            return check_allowed(['anyone'], request)
+        elif request.method == 'POST':
+            return check_allowed(['authenticated'], request)
         else:
             return True
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj = None):
+        if request.method == 'GET':
+            return check_allowed(['anyone'], request, obj)
         if request.method == 'PUT':
-            return check_allowed([{put_users}], request, obj)
+            return check_allowed(['owner'], request, obj)
         elif request.method == 'DELETE':
-            return check_allowed([{delete_users}], request, obj)
+            return check_allowed(['owner'], request, obj)
         else:
             return False
