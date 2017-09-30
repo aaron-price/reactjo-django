370416d34d50ef054e368204604e2bb72ea1d107
Aaron Price <coding.aaronp@gmail.com>
2017-09-30 10:19:16 -0700

    Add owner field to user model

--- assets/models/UserProfile.py
+++ assets/models/UserProfile.py
@@ -4,7 +4,9 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an email address.')
 
         email = self.normalize_email(email)
-        user = self.model(assignment_list)
+        owner = name
+        user = self.model(assignment_list, owner=owner)
+
 
         user.set_password(password)
         user.save(using=self._db)
@@ -12,7 +14,8 @@ class UserProfileManager(BaseUserManager):
         return user
 
     def create_superuser(self, title_list, password):
-        user = self.create_user(title_list, password)
+        owner = name
+        user = self.create_user(title_list, owner, password)
         user.is_superuser = True
         user.is_staff = True
 
