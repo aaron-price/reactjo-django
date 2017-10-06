ce535fd37416857b2cf05cb72c1b880b12730aba
Aaron Price <coding.aaronp@gmail.com>
2017-10-06 12:05:25 -0700

    Fix tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,18 +1,18 @@
-# from tests.config_mock import setup_config, teardown_config
-# import os
-# from helpers.config_manager import get_cfg, set_cfg
-#
-# def test_get_cfg():
-# 	setup_config()
-# 	cfg = get_cfg()
-# 	assert('paths' in cfg)
-#
-# def test_set_cfg():
-# 	cfg = get_cfg()
-# 	cfg['foo'] = 'bar'
-# 	set_cfg(cfg)
-#
-# 	another_cfg = get_cfg()
-# 	assert('foo' in another_cfg)
-#
-# 	teardown_config()
+from tests.config_mock import setup_config, teardown_config
+import os
+from helpers.config_manager import get_cfg, set_cfg
+
+def test_get_cfg():
+	setup_config()
+	cfg = get_cfg()
+	assert('paths' in cfg)
+
+def test_set_cfg():
+	cfg = get_cfg()
+	cfg['foo'] = 'bar'
+	set_cfg(cfg)
+
+	another_cfg = get_cfg()
+	assert('foo' in another_cfg)
+
+	teardown_config()
